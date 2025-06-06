e241681d9f2be544a485b786590b36f6e05a391f
Abdul Shakoor <ibupoto@gmail.com>
2025-06-07 00:42:23 +0500

    .....

--- app/page.js
+++ app/page.js
@@ -1,34 +1,38 @@
 "use client"
-import React,{useState} from 'react'
+import React, {useState} from 'react'
 import axios from 'axios'
 const Page = ()=>{
-const [images, setimages] = useState(['']);
-const getImages = async ()=>{
-  try {
-    const response = await axios.get("https://picsum.photos/v2/list");
-    const data = response.data;
-    console.log(data);
-    setimages(data);
-    
-  } catch (error) {
-    console.log("Error") 
-  }}
-
+  const [images, setimages] = useState([''])
+  const getImages = async ()=>{
+    try {
+      const response = await axios.get("https://picsum.photos/v2/list");
+      const data = response.data;
+      setimages(data);
+      console.log(data);
+    } catch (error) {
+      
+    }
+  }
   return(
     <>
-    <button onClick={getImages}> Images</button>
-    {images.map((elum,i)=>{
-      return
-      <img
-      key={i}
-      src={elum.download_url}
-      width={500}
-      height={600}
-      
-      />
-    })}
+    <button 
+    className='px-10 py-10 bg-green-600 mt-50 ml-50 text-white'
+    onClick={getImages}>Imges</button>
+    <div>
+      {images.map((elum,i)=>{
+        return <img
+        key={i}
+        src={elum.download_url}
+        width={400}
+        height={400}
+        className='p-4 inline-block'      
+        
+        
+        />
+      })}
+    </div>
+    
     </>
-    )
+  )
 }
-
 export default Page;
