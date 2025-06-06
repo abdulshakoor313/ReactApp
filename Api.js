66b2fb95fb2f84fc50dda3aa3a1de048fa0f2872
Abdul Shakoor <ibupoto@gmail.com>
2025-06-06 19:36:58 +0500

    updated and practiced more

---
+++ Api.js
@@ -0,0 +1,42 @@
+  "use client"
+
+  import axios from 'axios'
+  import React,{useState} from 'react'
+
+  const page = () => {
+    const [images, setimages] = useState([''])
+    const getImages = async()=>{
+  try {
+    const response = await axios.get("https://picsum.photos/v2list");
+    const data = response.data;
+    console.log(images);
+    setimages(data);
+      } catch (error) {
+        console.error("Error to fetch data");
+        
+      }
+    }
+    return (
+      <>
+      <div>
+        <button onClick={getImages} className='px-10 py-10 bg-green-600 mt-50 ml-50 text-white'>Get images</button>
+        <div className='p-10'>
+          {images.map((elum,i)=>{
+           return <img
+          key={i}
+          src={elum.download_url}
+          width={300}
+          height={300}
+          className='m-10 rounded inline-block  '
+           
+           
+           />
+          })}
+        </div>
+      </div>
+      
+      </>
+    )
+  }
+
+  export default page;
--- app/page.js
+++ app/page.js
@@ -1,42 +1,44 @@
-  "use client"
-
-  import axios from 'axios'
-  import React,{useState} from 'react'
-
-  const page = () => {
-    const [images, setimages] = useState([''])
-    const getImages = async()=>{
+"use client"
+import axios from "axios"
+import React,{useState} from "react"
+const Page = ()=>{
+const [images, setgetImages] = useState([''])
+const getImages = async()=>{
   try {
     const response = await axios.get("https://picsum.photos/v2/list");
     const data = response.data;
-    console.log(images);
-    setimages(data);
-      } catch (error) {
-        console.error("Error to fetch data");
-        
-      }
-    }
-    return (
-      <>
-      <div>
-        <button onClick={getImages} className='px-10 py-10 bg-green-600 mt-50 ml-50 text-white'>Get images</button>
-        <div className='p-10'>
-          {images.map((elum,i)=>{
-           return <img
-          key={i}
-          src={elum.download_url}
-          width={300}
-          height={300}
-          className='m-10 rounded inline-block  '
-           
-           
-           />
-          })}
-        </div>
-      </div>
-      
-      </>
-    )
+    console.log(data);
+    setgetImages(data);
+
+  } catch (error) {
+    console.error("Error to fetch data");
   }
 
-  export default page;
+}
+
+
+
+
+
+
+  return(
+    <>
+    <button onClick={getImages} className='px-10 py-10 bg-green-600 mt-50 ml-50 text-white'>Images</button>
+    <div>
+      {images.map((elum,i)=>{
+        return <img
+        key={i}
+        src={elum.download_url}
+        width={400}
+        height={400}
+        className='m-10 rounded inline-block  '
+
+        
+        />
+      })}
+    </div>
+    </>
+
+  )
+}
+export default Page;
