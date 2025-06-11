5eb81fab34efa93a5b7b5081dffac450a561aad2
Abdul Shakoor <ibupoto@gmail.com>
2025-06-11 17:37:13 +0500

    .......

--- app/page.js
+++ app/page.js
@@ -1,37 +1,39 @@
 "use client"
 import React, {useState} from 'react'
 import axios from 'axios'
+
+
 const Page = ()=>{
   const [images, setimages] = useState([''])
   const getImages = async ()=>{
     try {
       const response = await axios.get("https://picsum.photos/v2/list");
       const data = response.data;
-      setimages(data);
       console.log(data);
+      setimages(data)
     } catch (error) {
-      
+      console.log("Error")
     }
+
   }
+
   return(
     <>
-    <button 
-    className='px-10 py-10 bg-green-600 mt-50 ml-50 text-white'
-    onClick={getImages}>Imges</button>
+    <button onClick={getImages}>Show Images</button>
     <div>
       {images.map((elum,i)=>{
         return <img
-        key={i}
-        src={elum.download_url}
+       key={i}
+       src={elum.download_url}
         width={400}
         height={400}
-        className='p-4 inline-block'      
+        className='m-10 rounded inline-block  '
+        
         
         
         />
       })}
     </div>
-    
     </>
   )
 }
