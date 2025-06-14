72d0ac928c476b304867a6cf297e9f02b60b7226
Abdul Shakoor <ibupoto@gmail.com>
2025-06-14 12:43:27 +0500

    first commit

--- Components/Header.js
+++ Components/Header.js
@@ -1,25 +1,44 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 import Link from 'next/link'
+import { Menu, X } from 'lucide-react' // You can install lucide-react or use any icon
 
-const Header = (props) => {
+const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false)
+
   return (
-    <>
-    <div className='h-16 bg-green-500 items-center flex justify-between'>
-      <div>
-        <img src='/App/favicon.ico' alt='logo'/>
-      </div>
-      <div className='flex gap-10 px-5'>
-        <Link href="/">Home</Link>
-        <Link href="/About">Abous me</Link>
-        <Link href="/Courses">Courses</Link>
-        <Link href="/Books">Books</Link>
-        <Link href="/Account">Account</Link>
-      </div>
-    </div>
+    <header className='bg-green-500'>
+      <div className='h-16 flex justify-between items-center px-4 md:px-8'>
+        <div>
+          <img src='/App/favicon.ico' alt='logo' className='h-10 w-10' />
+        </div>
 
-   
-    </>
+        {/* Desktop Menu */}
+        <nav className='hidden md:flex gap-6 text-white'>
+          <Link href="/">Home</Link>
+          <Link href="/About">About me</Link>
+          <Link href="/Courses">Courses</Link>
+          <Link href="/Books">Books</Link>
+          <Link href="/Account">Account</Link>
+        </nav>
 
+        {/* Mobile Hamburger Icon */}
+        <button onClick={() => setMenuOpen(!menuOpen)} className='md:hidden text-white'>
+          {menuOpen ? <X size={28} /> : <Menu size={28} />}
+        </button>
+      </div>
+
+      {/* Mobile Menu */}
+      {menuOpen && (
+        <nav className='md:hidden flex flex-col gap-4 bg-green-600 px-4 py-3 text-white'>
+          <Link href="/">Home</Link>
+          <Link href="/About">About me</Link>
+          <Link href="/Courses">Courses</Link>
+          <Link href="/Books">Books</Link>
+          <Link href="/Account">Account</Link>
+        </nav>
+      )}
+    </header>
   )
 }
 
--- app/Account/page.js
+++ app/Account/page.js
@@ -32,6 +32,16 @@ const Account = () => {
       required
     />
   </div>
+    <div>
+    <label className="block text-sm font-medium text-gray-700" htmlFor="email">Password</label>
+    <input
+      type="password"
+      id="password"
+      name="password"
+      className="mt-1 block w-full rounded-lg border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500"
+      required
+    />
+  </div>
 
   <div>
     <label className="block text-sm font-medium text-gray-700" htmlFor="message">Message</label>
